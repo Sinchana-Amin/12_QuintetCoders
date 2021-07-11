5874c7153f5d2518d899b264c9ca8c4e46138d7a
Sinchana-Amin <sinchana.amin1222@gmail.com>
2021-07-11 09:46:33 +0530

    grocery stores

---
+++ GroceryStore/app/src/main/java/com/quintus/labs/grocerystore/activity/SignUp.java
@@ -0,0 +1,76 @@
+//SignUp.java
+package com.quintus.labs.grocerystore.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.quintus.labs.grocerystore.R;
+import com.quintus.labs.grocerystore.fragment.LoginFragment;
+import com.quintus.labs.grocerystore.fragment.SignUpFragment;
+import com.quintus.labs.grocerystore.util.Utils;
+
+public class SignUp extends AppCompatActivity {
+    private static FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+        getSupportActionBar().hide();
+        fragmentManager = getSupportFragmentManager();
+
+        // If savedinstnacestate is null then replace signup fragment
+        if (savedInstanceState == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frameContainer, new SignUpFragment(),
+                            Utils.SignUp_Fragment).commit();
+        }
+
+        // On close icon click finish activity
+        findViewById(R.id.close_activity).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View arg0) {
+                        finish();
+
+                    }
+                });
+
+    }
+
+    // Replace Login Fragment with animation
+    public void replaceLoginFragment() {
+        fragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.left_enter, R.anim.right_out)
+                .replace(R.id.frameContainer, new LoginFragment(),
+                        Utils.Login_Fragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // Find the tag of signup and forgot password fragment
+        Fragment SignUp_Fragment = fragmentManager
+                .findFragmentByTag(Utils.SignUp_Fragment);
+        Fragment ForgotPassword_Fragment = fragmentManager
+                .findFragmentByTag(Utils.ForgotPassword_Fragment);
+
+        // Check if both are null or not
+        // If both are not null then replace login fragment else do backpressed
+        // task
+
+        if (SignUp_Fragment != null)
+            replaceLoginFragment();
+        else if (ForgotPassword_Fragment != null)
+            replaceLoginFragment();
+        else
+            super.onBackPressed();
+    }
+}
